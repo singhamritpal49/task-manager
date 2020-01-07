425849a681269f8805778bc4234902afe74f6974
Amritpal Singh <singhamritpal49@gmail.com>
2020-01-06 23:46:34 -0500

    Connected mongodb

---
+++ mongodb.js
@@ -0,0 +1,20 @@
+// CRUD
+
+const mongodb = require('mongodb')
+const MongoClient = mongodb.MongoClient;
+
+
+const connectionURL = 'mongodb://127.0.0.1:27017'
+const databaseName = 'task-manager'
+
+MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
+    if (error) {
+        return console.log("Unable to connect to Database")
+    }
+    const db = client.db(databaseName)
+
+    db.collection('users').insertOne({
+        name: "amrit",
+        age: 222
+    })
+})
