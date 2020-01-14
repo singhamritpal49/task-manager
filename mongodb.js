fc1f4a5ce2f4ad96ff1346d97101c3c67c3138b5
Amritpal Singh <singhamritpal49@gmail.com>
2020-01-14 00:44:34 -0500

    mongodb objectID

--- mongodb.js
+++ mongodb.js
@@ -1,18 +1,26 @@
 // CRUD
 
-const mongodb = require('mongodb')
-const MongoClient = mongodb.MongoClient;
+
+//object destructure
+const { MongoClient, ObjectID } = require('mongodb')
+
 // This is to start connection 
-
-
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
+
+const id = new ObjectID()
+console.log(id.id) 
+// console.log(id.getTimestamp())
+// console.log(id.toHexString().length)
+
+
+
 
 // Connect takes 
 // first arg connection URL
 // second arg useNewUrlParser;
 // last callback function to connect to dB
-MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
+MongoClient.connect(connectionURL, { useNewUrlParser: true,  useUnifiedTopology: true  }, (error, client) => {
     if (error) {
         return console.log("Unable to connect to Database")
     }
@@ -21,7 +29,8 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
     // Creating USER DB
     // insert One 
     // db.collection('users').insertOne({
-    //     name: "HELLO",
+    //     _id: id,
+    //     name: "Vikram",
     //     age: 222
     // }, (error,result) => {
     //     if(error) {
@@ -53,32 +62,32 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
     // })
 
     // challenge 
-    db.collection('tasks').insertMany([
+    // db.collection('tasks').insertMany([
 
-     {
-         description: "TEST1",
-         completed: false
+    //  {
+    //      description: "TEST1",
+    //      completed: false
 
-     },
-     {
-        description: "TEST2",
-        completed: true
+    //  },
+    //  {
+    //     description: "TEST2",
+    //     completed: true
 
-     },
-     {
-        description: "TEST3",
-        completed: false
+    //  },
+    //  {
+    //     description: "TEST3",
+    //     completed: false
 
-     }
+    //  }
 
 
 
-    ], (error,result) => {
-        if(error) {
-            return console.log("Unable to insert")
-        }
-        console.log(result.ops)
-    })
+    // ], (error,result) => {
+    //     if(error) {
+    //         return console.log("Unable to insert")
+    //     }
+    //     console.log(result.ops)
+    // })
 
 
 })
