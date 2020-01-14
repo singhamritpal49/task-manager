f4c92d4b37930d91015db979f3e1066819981ad6
Amritpal Singh <singhamritpal49@gmail.com>
2020-01-14 01:55:40 -0500

    mondo UPDATE

--- mongodb.js
+++ mongodb.js
@@ -1,4 +1,4 @@
-// CRUD CREATE READ DELETE UPDATE DELETE
+// C R U D ----> CREATE READ DELETE UPDATE DELETE <----
 
 
 //object destructure
@@ -14,37 +14,42 @@ const databaseName = 'task-manager'
 // first arg connection URL
 // second arg useNewUrlParser;
 // last callback function to connect to dB
-MongoClient.connect(connectionURL, { useNewUrlParser: true,  useUnifiedTopology: true  }, (error, client) => {
+MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
     if (error) {
         return console.log("Unable to connect to Database")
     }
     const db = client.db(databaseName)
-    
 
-    // db.collection('users').findOne({ age: 78}, (error,user) => {
-    //     if(error) {
-    //         return console.log("unable to fetch")
+
+    // const updatePromise = db.collection('users').updateOne({
+    //     _id: new ObjectID("5e140bbb8b895b1a41b33eb2")
+    // }, {
+    //     // $set: {
+    //     //     name: 'Mike'
+    //     // }
+    //     $inc: {
+    //         age: 1
     //     }
-    //     // console.log(user)
+
+    // })
+    // updatePromise.then((result) =>{
+    //     console.log(result)
+    // }).catch((error) => {
+    //     console.log(error)
     // })
 
-    // db.collection('users').find({ age: 25 }).toArray((error,users) => {
-    //     console.log(users)
-    // })
-    // db.collection('users').find({ age: 25 }).toArray((error,count) => {
-    //     console.log(count)
-    // })
-
-    // db.collection('tasks').find({completed: false}).toArray((error,tasks) => {
-    //     console.log(tasks)
-    // })
-
-    db.collection('tasks').findOne({ _id: new ObjectID("5e1d50310fbc020bf50a044e")}, (error,task) => {
-        if(error) {
-            return console.log("unable to fetch")
+    const updatePromise = db.collection('tasks').updateMany({
+       completed: true
+    }, {
+        $set: {
+            completed: false
         }
-        console.log(task)
+
     })
-    
+    updatePromise.then((result) =>{
+        console.log(result)
+    }).catch((error) => {
+        console.log(error)
+    })
 
 })
