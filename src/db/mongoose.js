ddf44a34470d3c957bc7628f1be07eb2c9cfcedd
Amritpal Singh <singhamritpal49@gmail.com>
2020-01-15 02:12:55 -0500

    added APIEND Points

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -3,5 +3,6 @@ const moongoose = require('mongoose')
 moongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true, 
     useUnifiedTopology: true,
-    useCreateIndex: true
+    useCreateIndex: true,
+    useFindAndModify: false
 })
