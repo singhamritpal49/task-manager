bbedb51135223d52df657cd3e22fdbb2bd3d0125
Amritpal Singh <singhamritpal49@gmail.com>
2020-02-01 16:25:44 -0500

    Added Paginating -> Limiting & Skipping

--- src/routers/task.js
+++ src/routers/task.js
@@ -71,6 +71,7 @@ router.get('/tasks/:id', auth , async (req, res) => {
 
 })
 // GET /tasks?completed=true
+// GET /tasks?limit=10&skip=10
 router.get('/tasks', auth , async (req, res) => {
     const match = {}
     if(req.query.completed) {
@@ -80,7 +81,11 @@ router.get('/tasks', auth , async (req, res) => {
     try {
         await req.user.populate({
             path: 'tasks',
-            match
+            match,
+            options: {
+                limit: parseInt(req.query.limit),
+                skip:  parseInt(req.query.skip)
+            }
         }).execPopulate()
         res.send(req.user.tasks)
     } catch (error) {
