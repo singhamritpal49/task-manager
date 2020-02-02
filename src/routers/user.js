cd427c96e3364c583aaade1edaff3509ba11be83
Amritpal Singh <singhamritpal49@gmail.com>
2020-02-01 22:45:05 -0500

    added file validations for User Profile Picture

--- src/routers/user.js
+++ src/routers/user.js
@@ -90,12 +90,23 @@ router.delete('/users/me', auth, async (req, res) => {
     }
 })
 const upload = multer({
-    dest: 'avatar'
+    dest: 'avatar',
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter(req,file,cb) {
+        if(!file.originalname.match(/\.(png|jpg|jpeg)$/)) { 
+            return cb(new Error("Please Upload a Image Format"))
+        }
+        cb(undefined,true)
+    }
 })
 
 router.post('/users/me/avatar', upload.single('avatar'), (req,res) => {
     res.send()
 })
+
+
 
 
 
