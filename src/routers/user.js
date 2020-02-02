f54ec29993f09da47468561a6beaa427c010580d
Amritpal Singh <singhamritpal49@gmail.com>
2020-02-01 23:47:21 -0500

    Added ability to fetch user profile in front end

--- src/routers/user.js
+++ src/routers/user.js
@@ -16,10 +16,7 @@ router.post('/users', async (req, res) => {
     } catch (error) {
         res.status(400).send(error)
     }
-
 })
-
-
 
 router.post('/users/login', async (req, res) => {
 
@@ -33,7 +30,7 @@ router.post('/users/login', async (req, res) => {
 });
 
 router.post('/users/logout', auth, async (req, res) => {
-
+    
     try {
         req.user.tokens = req.user.tokens.filter((token) => {
             return token.token !== req.token
@@ -93,28 +90,41 @@ const upload = multer({
     limits: {
         fileSize: 1000000
     },
-    fileFilter(req,file,cb) {
-        if(!file.originalname.match(/\.(png|jpg|jpeg)$/)) { 
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match(/\.(png|jpg|jpeg)$/)) {
             return cb(new Error("Please Upload a Image Format"))
         }
-        cb(undefined,true)
+        cb(undefined, true)
     }
 })
 
-router.post('/users/me/avatar', auth , upload.single('avatar'), async (req,res) => {
+router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
     req.user.avatar = req.file.buffer
     await req.user.save()
     res.send()
-},(error,req,res,next)=> {
-    res.status(400).send({error: error.message})
+}, (error, req, res, next) => {
+    res.status(400).send({ error: error.message })
 })
 
-router.delete('/users/me/avatar', auth, async (req,res) => {
-    
-        req.user.avatar = undefined;
-        await req.user.save()
-        res.send()
-    
+router.delete('/users/me/avatar', auth, async (req, res) => {
+
+    req.user.avatar = undefined;
+    await req.user.save()
+    res.send()
+})
+
+router.get('/users/:id/avatar', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id)
+
+        if (!user || !user.avatar) {
+            throw new Error()
+        }
+        res.set('Content-type', 'image/jpg')
+        res.send(user.avatar)
+    } catch (error) {
+        res.status(404)
+    }
 })
 
 
