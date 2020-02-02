c6069bc233d1b6b55997d95717d702cea8044ced
Amritpal Singh <singhamritpal49@gmail.com>
2020-02-02 00:02:25 -0500

    Auto cropping profile image and covert image format to PNG

--- src/index.js
+++ src/index.js
@@ -1,46 +1,14 @@
 const express = require('express');
 require('./db/mongoose');
-const User = require('./models/user');
-const Task = require('./models/task');
 const userRouter = require('../src/routers/user');
 const taskRouter = require('../src/routers/task');
-
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-
-const multer = require('multer')
-const upload = multer({
-    dest: 'images',
-    limits: {
-        fileSize: 1000000
-    },
-    fileFilter(req, file, cb) {
-        if (!file.originalname.match(/\.(doc|docx)$/)) {
-            return cb(new Error('Please upload a Word Document'))
-        }
-        cb(undefined, true)
-
-    }
-})
-
-
-app.post('/upload', upload.single('upload'), (req, res) => {
-    res.send()
-}, (error, req, res, next) => {
-    res.status(400).send({ error: error.message })
-})
-
-
-
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
-
-
-const jwt = require('jsonwebtoken');
-
 
 app.listen(port, () => {
     console.log("Sever is up at " + port)
--- src/routers/user.js
+++ src/routers/user.js
@@ -3,7 +3,7 @@ const auth = require('../middleware/auth')
 const router = new express.Router()
 const User = require('../models/user')
 const multer = require('multer')
-
+const sharp = require('sharp')
 
 router.post('/users', async (req, res) => {
     const user = new User(req.body)
@@ -30,7 +30,7 @@ router.post('/users/login', async (req, res) => {
 });
 
 router.post('/users/logout', auth, async (req, res) => {
-    
+
     try {
         req.user.tokens = req.user.tokens.filter((token) => {
             return token.token !== req.token
@@ -98,13 +98,18 @@ const upload = multer({
     }
 })
 
-router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
-    req.user.avatar = req.file.buffer
+router.post('/users/me/avatar', auth , upload.single('avatar'), async (req, res) => {
+    // Buffer is to convert to any format, resize you want
+    const buffer = await sharp(req.file.buffer).resize({ width: 250, height: 250 }).png().toBuffer()
+
+    req.user.avatar = buffer
     await req.user.save()
     res.send()
 }, (error, req, res, next) => {
     res.status(400).send({ error: error.message })
 })
+
+
 
 router.delete('/users/me/avatar', auth, async (req, res) => {
 
@@ -120,7 +125,7 @@ router.get('/users/:id/avatar', async (req, res) => {
         if (!user || !user.avatar) {
             throw new Error()
         }
-        res.set('Content-type', 'image/jpg')
+        res.set('Content-type', 'image/png')
         res.send(user.avatar)
     } catch (error) {
         res.status(404)
